3ee8bbfe0a566e6539a477665b5658658ee8b772
Robert Elliot <rob@lidalia.org.uk>
2018-02-13 17:45:55 +0000

    Improve result formatting

--- src/Sudoku/SimpleCli/ResultFormatting.hs
+++ src/Sudoku/SimpleCli/ResultFormatting.hs
@@ -5,5 +5,8 @@ import Sudoku.Interfaces
 format :: [Sudoku] -> String
 format [] = "Not a valid sudoku; no solutions possible"
 format [valid] = show valid
-format xs = let examples = take 5 xs
- in "Not a valid sudoku; at least " ++ show (length examples) ++ " solutions\n" ++ show examples
+format xs =
+  let examples = take 5 xs
+      numberOfExamples = length examples
+      text = if numberOfExamples < 5 then "" else "at least "
+  in "Not a valid sudoku; " ++ text ++ show numberOfExamples ++ " solutions\n" ++ show examples
