18f3b9c10eee1d9843d10a8f32c758da145f78dc
Robert Elliot <rob@lidalia.org.uk>
2018-02-18 17:50:00 +0000

    Document leftPartition

--- src/Ext/Data/Either.hs
+++ src/Ext/Data/Either.hs
@@ -15,6 +15,32 @@ leftMap :: (a -> c) -> Either a b -> Either c b
 leftMap f (Left x) = Left(f x)
 leftMap _ (Right x) = Right x
 
+-- | Turns a list of 'Either' into a single Either
+-- Returns either a 'Left' containing a list, in order, of all the 'Left'
+-- elements from the input or a 'Right' containing a list, in order, of all the
+-- 'Right' elements s from the input. If the input contains both Lefts and
+-- Rights, it returns the Lefts as a Left.
+--
+-- Intended for amalgamating a list of Eithers representing possible
+-- success / failure cases into a single success / failure case - hence any
+-- failure overrides all successes.
+--
+-- ==== __Examples__
+--
+-- Basic usage:
+--
+-- >>> let list = [ Left "foo", Left "bar", Left "baz" ]
+-- >>> leftPartition list
+-- Left ["foo","bar","baz"]
+--
+-- >>> let list = [ Right 3, Right 7 ]
+-- >>> leftPartition list
+-- Right [3,7]
+--
+-- >>> let list = [ Left "foo", Right 3, Left "bar", Right 7, Left "baz" ]
+-- >>> leftPartition list
+-- Left ["foo","bar","baz"]
+--
 leftPartition :: [Either a b] -> Either [a] [b]
 leftPartition xs = case partitionEithers xs of
     ([], bs) -> Right bs
